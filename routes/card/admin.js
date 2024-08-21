7d4d31aea87041d218f49294142d6c7be08db136
rednotsus <Jiayangc1@gmail.com>
2024-08-20 21:45:55 -0500

    add

--- routes/card/admin.js
+++ routes/card/admin.js
@@ -36,7 +36,7 @@ router.get("/add", async (req, res) => {
     statement: "Admin Deposit: ",
   });
 
-  res.json({
+  res.status(200).json({
     balance: clientCard.balance,
     id: clientCard.id,
     amount: amount,
